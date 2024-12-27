295bc22fe73d5b34bdc9c4fe6a910863bd893c19
marypaaatrici <marypaaatricia@gmail.com>
2024-12-27 15:35:38 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+from seleniumbase import SB
+
+with SB(test=True) as sb:
+    sb.open("https://google.com/ncr")
+    sb.type('[title="Search"]', "SeleniumBase GitHub page\n")
+    sb.click('[href*="github.com/seleniumbase/"]')
+    sb.save_screenshot_to_logs()  # ./latest_logs/
+    print(sb.get_page_title())
